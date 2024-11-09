307cfe5b3e78642950fb0361aabe5b782d43cfff
jrestall <james.restall@gmail.com>
2024-11-10 11:11:33 +1300

    test: add more import cases

--- packages/remix-to-react-router/__testfixtures__/imports.remix.tsx
+++ packages/remix-to-react-router/__testfixtures__/imports.remix.tsx
@@ -1,3 +1,23 @@
 // @ts-nocheck
 
-import { redirect } from "@remix-run/node";
+import { redirect } from '@remix-run/node';
+import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
+import {
+  json,
+  type LoaderFunctionArgs,
+  type HeadersFunction,
+  type LinksFunction,
+  type MetaFunction,
+} from '@remix-run/node';
+import {
+  Form,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useMatches,
+  useSubmit,
+} from '@remix-run/react';
--- packages/remix-to-react-router/__testfixtures__/imports.rr7.tsx
+++ packages/remix-to-react-router/__testfixtures__/imports.rr7.tsx
@@ -1,3 +1,23 @@
 // @ts-nocheck
 
 import { redirect } from "react-router";
+import type { AppLoadContext, EntryContext } from "react-router";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type HeadersFunction,
+  type LinksFunction,
+  type MetaFunction,
+} from "react-router";
+import {
+  Form,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useMatches,
+  useSubmit,
+} from "react-router";
