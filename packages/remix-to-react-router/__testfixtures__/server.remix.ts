ce833285001446d5396fdddf2048b46d9f0ca8b7
jrestall <james.restall@gmail.com>
2024-11-12 07:50:47 +1300

    chore: add ts-nocheck

--- packages/remix-to-react-router/__testfixtures__/server.remix.ts
+++ packages/remix-to-react-router/__testfixtures__/server.remix.ts
@@ -1,10 +1,12 @@
-import { createRequestHandler } from "@remix-run/express";
-import express from "express";
+// @ts-nocheck
+
+import { createRequestHandler } from '@remix-run/express';
+import express from 'express';
 
 const viteDevServer =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? undefined
-    : await import("vite").then((vite) =>
+    : await import('vite').then((vite) =>
         vite.createServer({
           server: { middlewareMode: true },
         })
@@ -17,29 +19,24 @@ if (viteDevServer) {
   app.use(viteDevServer.middlewares);
 } else {
   app.use(
-    "/assets",
-    express.static("build/client/assets", {
+    '/assets',
+    express.static('build/client/assets', {
       immutable: true,
-      maxAge: "1y",
+      maxAge: '1y',
     })
   );
 }
-app.use(express.static("build/client", { maxAge: "1h" }));
+app.use(express.static('build/client', { maxAge: '1h' }));
 
 // handle SSR requests
 app.all(
-  "*",
+  '*',
   createRequestHandler({
     build: viteDevServer
-      ? () =>
-          viteDevServer.ssrLoadModule(
-            "virtual:remix/server-build"
-          )
-      : await import("./build/server/index.js"),
+      ? () => viteDevServer.ssrLoadModule('virtual:remix/server-build')
+      : await import('./build/server/index.js'),
   })
 );
 
 const port = 3000;
-app.listen(port, () =>
-  console.log("http://localhost:" + port)
-);
+app.listen(port, () => console.log('http://localhost:' + port));
